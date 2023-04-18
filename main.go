ec57e4054b8727d3854a16969ad5325d3a190ab0
David Gubler <david.gubler@vshn.ch>
2023-04-18 17:04:43 +0200

    feat: Add support for fetching namespace labels

--- main.go
+++ main.go
@@ -1,20 +1,53 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"net/http"
 	_ "net/http/pprof"
 
 	log "github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 var (
 	version = "0.0.0"
 )
+
+func fetchTenantMap(clientset *kubernetes.Clientset) (map[string]string, error) {
+	namespaces, err := clientset.CoreV1().Namespaces().List(context.Background(), v1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	tenantMap := make(map[string]string)
+	for _, namespace := range namespaces.Items {
+		if org, found := namespace.Labels["appuio.io/organization"]; found {
+			tenantMap[namespace.Name] = org
+		}
+	}
+	return tenantMap, nil
+}
+
+func refreshTenantMap(p *processor, clientset *kubernetes.Clientset) {
+	for {
+		time.Sleep(60 * time.Second)
+		tenantMap, err := fetchTenantMap(clientset)
+		if err != nil {
+			log.Fatalf("error refreshing tenant map: %v", err)
+		} else {
+			p.TenantLookup.Store(&tenantMap)
+			log.Warnf("successfully refreshed tenant map, %d namespaces with tenants found", len(tenantMap))
+		}
+	}
+}
 
 func main() {
 	cfgFile := flag.String("config", "", "Path to a config file")
@@ -46,7 +79,32 @@ func main() {
 		log.SetLevel(lvl)
 	}
 
-	proc := newProcessor(*cfg)
+	clientConfig, err := rest.InClusterConfig()
+	if err != nil {
+		fmt.Printf("k8s_api: %s\n", cfg.K8s_api)
+		fmt.Printf("k8s_serviceaccount: %s\n", cfg.K8s_serviceaccount)
+		clientConfig = &rest.Config{}
+		clientConfig.BearerToken = cfg.K8s_token
+		clientConfig.Host = cfg.K8s_api
+	}
+
+	clientset, err := kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		fmt.Printf("error getting Kubernetes clientset: %v\n", err)
+		os.Exit(1)
+	}
+
+	tenantMap, err := fetchTenantMap(clientset)
+	if err != nil {
+		fmt.Printf("error getting initial tenant map: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("tenant lookup map: %v\n", tenantMap)
+
+	proc := newProcessor(*cfg, tenantMap)
+
+	go refreshTenantMap(proc, clientset)
 
 	if err = proc.run(); err != nil {
 		log.Fatalf("Unable to start: %s", err)
